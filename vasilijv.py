4af05222bd93d98b02926a12db769cb8a893b118
Василий <vasilij.golnev@yandex.ru>
2023-12-22 19:51:02 +0700

    code

--- vasilijv.py
+++ vasilijv.py
@@ -1,1 +1,13 @@
-fgjnmsxfgjhsghmdx
+string = 'iteration string'
+count = 0
+while count < len(string):
+    print(string[count])
+    count += 1
+
+tuPle = (1, 4, 6, 'tuple')
+while count < len(tuPle):
+    print(tuPle[count])
+    count += 1
+
+lisT = [4, 6, 4,]
+print(string[1: 7])
